0732499224cdc61662c61a71cb1a1f37bc1cd935
hùng lý bá <libahu1809@gmail.com>
2026-09-13 17:05:41 +0700

    fix wb

--- main.user.js
+++ main.user.js
@@ -7,16 +7,16 @@
 // @match        *://*.bitheroesgame.com/*
 // @run-at       document-start
 // @grant        none
-// @require      https://cdn.jsdelivr.net/gh/laviehihi/bh-scripts@v1.0.16/core/utils.js
-// @require      https://cdn.jsdelivr.net/gh/laviehihi/bh-scripts@v1.0.16/core/rules.js
-// @require      https://cdn.jsdelivr.net/gh/laviehihi/bh-scripts@v1.0.16/core/pixel.js
-// @require      https://cdn.jsdelivr.net/gh/laviehihi/bh-scripts@v1.0.16/core/speed-hack.js
-// @require      https://cdn.jsdelivr.net/gh/laviehihi/bh-scripts@v1.0.16/core/engine.js
-// @require      https://cdn.jsdelivr.net/gh/laviehihi/bh-scripts@v1.0.16/ui/overlay.js
-// @require      https://cdn.jsdelivr.net/gh/laviehihi/bh-scripts@v1.0.16/ui/help.js
-// @require      https://cdn.jsdelivr.net/gh/laviehihi/bh-scripts@v1.0.16/ui/marker.js
-// @require      https://cdn.jsdelivr.net/gh/laviehihi/bh-scripts@v1.0.16/ui/addmode.js
-// @require      https://cdn.jsdelivr.net/gh/laviehihi/bh-scripts@v1.0.16/wb-party.js
+// @require      https://cdn.jsdelivr.net/gh/laviehihi/bh-scripts@v1.0.17/core/utils.js
+// @require      https://cdn.jsdelivr.net/gh/laviehihi/bh-scripts@v1.0.17/core/rules.js
+// @require      https://cdn.jsdelivr.net/gh/laviehihi/bh-scripts@v1.0.17/core/pixel.js
+// @require      https://cdn.jsdelivr.net/gh/laviehihi/bh-scripts@v1.0.17/core/speed-hack.js
+// @require      https://cdn.jsdelivr.net/gh/laviehihi/bh-scripts@v1.0.17/core/engine.js
+// @require      https://cdn.jsdelivr.net/gh/laviehihi/bh-scripts@v1.0.17/ui/overlay.js
+// @require      https://cdn.jsdelivr.net/gh/laviehihi/bh-scripts@v1.0.17/ui/help.js
+// @require      https://cdn.jsdelivr.net/gh/laviehihi/bh-scripts@v1.0.17/ui/marker.js
+// @require      https://cdn.jsdelivr.net/gh/laviehihi/bh-scripts@v1.0.17/ui/addmode.js
+// @require      https://cdn.jsdelivr.net/gh/laviehihi/bh-scripts@v1.0.17/wb-party.js
 // ==/UserScript==
 
 (function () {
